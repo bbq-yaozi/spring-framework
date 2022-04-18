ed4b16021c7e37d45c6c902b9a3e90e238ade3c4
bbq-yaozi <bbq_yaozi@163.com>
2022-04-18 19:31:05 +0800

    add BeanFactory Demo

--- yaozi-demo/src/main/java/com/yaozi/demo/MyApplication.java
+++ yaozi-demo/src/main/java/com/yaozi/demo/MyApplication.java
@@ -1,13 +1,19 @@
 package com.yaozi.demo;
 
 import com.yaozi.demo.beans.Hello;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
 
 public class MyApplication {
 	public static void main(String[] args) {
-		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
-		Hello bean = applicationContext.getBean(Hello.class);
+		ApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+		Hello bean = context.getBean(Hello.class);
 		bean.sayHello();
+
+		XmlBeanFactory factory = new XmlBeanFactory(new ClassPathResource("applicationContext.xml"));
+		Hello bean1 = factory.getBean(Hello.class);
+		bean1.sayHello();
 	}
 }
